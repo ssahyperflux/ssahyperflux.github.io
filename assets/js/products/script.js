15e7775642d5abd19eae81099ff105d1e459f757
devsai9 <saisiddhish@gmail.com@gmail.com>
2022-09-16 17:42:23 -0400

    link products to products page

---
+++ assets/js/products/script.js
@@ -0,0 +1,11 @@
+$('#small').click(function() {
+    window.location.href = 'bladeless-gaming-fan.html?size=small';
+});
+
+$('#medium').click(function() {
+    window.location.href = 'bladeless-gaming-fan.html?size=medium';
+});
+
+$('#large').click(function() {
+    window.location.href = 'bladeless-gaming-fan.html?size=large';
+});
